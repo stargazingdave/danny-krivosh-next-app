4c783ec6780c24322b1ac19c7085c7c2394aa95c
David Portal <davidportal91@gmail.com>
2025-03-24 21:10:08 +0200

    No code changes detected; skipping commit.

--- app/components/Snake/SnakeGame.tsx
+++ app/components/Snake/SnakeGame.tsx
@@ -4,12 +4,14 @@ import { useAppContext } from "@/app/AppContext";
 import React, { useEffect, useState } from "react";
 import { FaWineBottle } from "react-icons/fa6";
 import { HiX } from "react-icons/hi";
+import { TbPillFilled } from "react-icons/tb";
 
 const GRID_HEIGHT = 20;
 const GRID_WIDTH = 36;
 const CELL_SIZE = 20; // Pixels per cell
-const INITIAL_SPEED = 70; // Movement speed in ms
-const MAX_SPEED = 100;
+const INITIAL_SPEED = 1; // Movement speed in ms
+const MAX_SPEED = 10;
+const MIN_SPEED = 0.2;
 
 type Direction = "UP" | "DOWN" | "LEFT" | "RIGHT";
 
@@ -18,19 +20,79 @@ const getRandomPosition = () => ({
     y: Math.floor(Math.random() * GRID_HEIGHT),
 });
 
+const getRandomBottle = () => Math.floor(Math.random() * 8) === 5;
+const getRandomPill = () => Math.floor(Math.random() * 8) === 5;
+
+// Function to get any random color possible
+const getRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+interface snakeColor {
+    head: string;
+    body: string;
+}
+
+interface SnakeColorMap {
+    [key: string]: snakeColor;
+}
+
+const snakeColorMap: SnakeColorMap = {
+    foodEaten: {
+        head: 'white',
+        body: '#777777',
+    },
+    bottleEaten: {
+        head: '#00ff22d1',
+        body: '#f7a60d',
+    },
+    pillEaten: {
+        head: getRandomColor(),
+        body: getRandomColor(),
+    },
+    default: {
+        head: '#777777',
+        body: '#333333',
+    }
+}
+
+type EventType = 'foodEaten' | 'bottleEaten' | 'pillEaten';
+
+type ScoreType = 'food' | 'bottle' | 'pill';
+interface Score {
+    score: number;
+    color: string;
+}
+
+const scoreMap: Record<ScoreType, Score> = {
+    food: { score: 1, color: '#d7b964' },
+    bottle: { score: 50, color: '#00ff22d1' },
+    pill: { score: 15, color: '#590b02' },
+}
+
 export const SnakeGame: React.FC = () => {
     const {
         setSnakeOpen
     } = useAppContext();
+
+    const [gameStatus, setGameStatus] = useState<'ready' | 'running' | 'gameOver'>('ready');
     const [snake, setSnake] = useState([{ x: 10, y: 10 }]);
     const [food, setFood] = useState(getRandomPosition);
     const [direction, setDirection] = useState<Direction>("RIGHT");
-    const [isGameOver, setIsGameOver] = useState(false);
-    const [foodEaten, setFoodEaten] = useState<boolean>(false);
-    const [bottleEaten, setBottleEaten] = useState<boolean>(false);
+    const [latestEvent, setLatestEvent] = useState<EventType | null>(null);
     const [speed, setSpeed] = useState<number>(INITIAL_SPEED);
     const [bottle, setBottle] = useState(getRandomPosition);
-    const [countForBottle, setCountForBottle] = useState<number>(0);
+    const [showBottle, setShowBottle] = useState(false);
+    const [score, setScore] = useState(0);
+    const [newScore, setNewScore] = useState<Score | null>(null);
+    const [pill, setPill] = useState(getRandomPosition);
+    const [showPill, setShowPill] = useState(false);
+    const [snakeColor, setSnakeColor] = useState<snakeColor>(snakeColorMap.default);
 
     useEffect(() => {
         const handleKeyDown = (e: KeyboardEvent) => {
@@ -44,7 +106,10 @@ export const SnakeGame: React.FC = () => {
                 a: "LEFT",
                 d: "RIGHT",
             };
-            if (keyMap[e.key] && !isGameOver) {
+            if (e.key !== 'Escape' && gameStatus === 'ready') {
+                setGameStatus('running');
+            }
+            if (keyMap[e.key] && gameStatus === 'running') {
                 setDirection((prev) =>
                     keyMap[e.key] === "UP" && prev === "DOWN"
                         ? prev
@@ -56,14 +121,22 @@ export const SnakeGame: React.FC = () => {
                                     ? prev
                                     : keyMap[e.key]
                 );
-            }
+            };
+            if (gameStatus === 'gameOver') {
+                if (e.key === 'Escape') {
+                    setSnakeOpen(false);
+                }
+                if (e.key === 'Enter') {
+                    restartGame();
+                }
+            };
         };
         window.addEventListener("keydown", handleKeyDown);
         return () => window.removeEventListener("keydown", handleKeyDown);
-    }, [isGameOver]);
+    }, [gameStatus]);
 
     useEffect(() => {
-        if (isGameOver) return;
+        if (gameStatus === 'gameOver' || gameStatus === 'ready') return;
         const moveSnake = () => {
             setSnake((prevSnake) => {
                 const newSnake = [...prevSnake];
@@ -91,13 +164,13 @@ export const SnakeGame: React.FC = () => {
                     head.x >= GRID_WIDTH ||
                     head.y >= GRID_HEIGHT
                 ) {
-                    setIsGameOver(true);
+                    setGameStatus('gameOver');
                     return prevSnake;
                 }
 
                 // Check collision with itself
-                if (newSnake.some((segment) => segment.x === head.x && segment.y === head.y)) {
-                    setIsGameOver(true);
+                if (newSnake.some((segment, index) => index !== 1 && index !== 2 && segment.x === head.x && segment.y === head.y)) {
+                    setGameStatus('gameOver');
                     return prevSnake;
                 }
 
@@ -106,54 +179,137 @@ export const SnakeGame: React.FC = () => {
 
                 // Check if food eaten
                 if (head.x === food.x && head.y === food.y) {
-                    const newSpeed = speed + 5 > MAX_SPEED ? MAX_SPEED : speed + 5;
+                    const newSpeed = ((speed + 0.5) > MAX_SPEED) ? MAX_SPEED : (speed + 0.5);
                     setFood(getRandomPosition());
                     setSpeed(newSpeed);
-                    setCountForBottle((prev) => prev + 1);
-                    setFoodEaten(true);
-                    setTimeout(() => setFoodEaten(false), 1000);
+                    setLatestEvent('foodEaten');
+                    setTimeout(() => setLatestEvent(null), 1000);
+                    if (!showBottle) setShowBottle(getRandomBottle());
+                    if (!showPill) setShowPill(getRandomPill());
                     console.log(`New speed: ${newSpeed}`);
                 } else {
                     newSnake.pop();
                 }
 
                 // Check if bottle eaten
-                if (head.x === bottle.x && head.y === bottle.y) {
-                    const newSpeed = Math.floor(Math.random() * (MAX_SPEED - INITIAL_SPEED + 1)) + INITIAL_SPEED;
+                if (head.x === bottle.x && head.y === bottle.y && showBottle) {
                     setBottle(getRandomPosition());
-                    setCountForBottle(0);
-                    setSpeed(newSpeed);
-                    setBottleEaten(true);
-                    setTimeout(() => setBottleEaten(false), 1000);
-                    console.log(`New speed: ${newSpeed}`);
+                    setLatestEvent('bottleEaten');
+                    setShowBottle(false);
+                    setTimeout(() => setLatestEvent(null), 1000);
                 }
+
+                // Check if pill eaten
+                if (head.x === pill.x && head.y === pill.y && showPill) {
+                    setPill(getRandomPosition());
+                    setLatestEvent('pillEaten');
+                    setShowPill(false);
+                    setTimeout(() => setLatestEvent(null), 1000);
+                }
+
+                getSnakeCellColor();
 
                 return newSnake;
             });
         };
 
-        const interval = setInterval(moveSnake, MAX_SPEED - speed);
+        const interval = setInterval(moveSnake, (MAX_SPEED - speed) * 10);
         return () => clearInterval(interval);
-    }, [direction, food, isGameOver]);
+    }, [direction, food, gameStatus]);
+
+    // Update speed when bottle is eaten
+    useEffect(() => {
+        const head = { ...snake[0] };
+        // Check if bottle eaten
+        if (head.x === bottle.x && head.y === bottle.y && showBottle) {
+            const newSpeed = Math.floor(Math.random() * (MAX_SPEED - MIN_SPEED + 1)) + MIN_SPEED;
+            setSpeed(newSpeed);
+            console.log(`New speed: ${newSpeed}`);
+        }
+    }, [showBottle]);
+
+    // Update speed when pill is eaten
+    useEffect(() => {
+        const head = { ...snake[0] };
+        // Check if pill eaten
+        if (head.x === pill.x && head.y === pill.y && showPill) {
+            setSpeed((prev) => prev - 2);
+            console.log(`New speed: ${speed}`);
+        }
+    }, [showPill]);
+
+    useEffect(() => {
+        if (latestEvent === 'foodEaten') {
+            setScore((prev) => prev + scoreMap.food.score);
+            setNewScore(scoreMap.food);
+
+        }
+        if (latestEvent === 'bottleEaten') {
+            setScore((prev) => prev + scoreMap.food.score);
+            setNewScore(scoreMap.bottle);
+            setTimeout(() => setNewScore(null), 3000);
+        }
+        if (latestEvent === 'pillEaten') {
+            setScore((prev) => prev + scoreMap.food.score);
+            setNewScore(scoreMap.pill);
+            setTimeout(() => setNewScore(null), 3000);
+        }
+    }, [latestEvent]);
 
     const restartGame = () => {
         setSnake([{ x: 10, y: 10 }]);
         setFood(getRandomPosition());
         setDirection("RIGHT");
-        setIsGameOver(false);
+        setGameStatus('running');
         setSpeed(INITIAL_SPEED);
         setBottle(getRandomPosition());
-        setCountForBottle(0);
     };
+
+    const getSnakeCellColor = () => {
+        if (latestEvent === 'foodEaten') {
+            setSnakeColor(snakeColorMap.foodEaten);
+            // setTimeout(() => setNewScore(null), 3000);
+            // setSnakeColor(snakeColorMap.default);
+        } else if (latestEvent === 'bottleEaten') {
+            setSnakeColor(snakeColorMap.bottleEaten);
+            // setTimeout(() => setNewScore(null), 3000);
+            // setSnakeColor(snakeColorMap.default);
+        } else if (latestEvent === 'pillEaten') {
+            setSnakeColor(snakeColorMap.pillEaten);
+            // setTimeout(() => setNewScore(null), 3000);
+            // setSnakeColor(snakeColorMap.default);
+        } else {
+            setSnakeColor(snakeColorMap.default);
+        }
+    }
 
     return (
         <div
-            className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-90 z-50"
-            onClick={() => setSnakeOpen(false)} // Clicking outside closes the game
+            className="fixed inset-0 flex items-center justify-center bg-black/90 backdrop-blur-sm z-50"
+            onClick={() => setSnakeOpen(false)}
         >
-            <div className="relative bg-gray-900 rounded-md shadow-lg" onClick={(e) => e.stopPropagation()}>
-                <div className="w-full h-fit flex items-center">
-                    <h2 className="w-full text-center text-2xl font-bold p-2 tracking-widest">
+            <div
+                className="relative bg-gray-900 rounded-2xl overflow-hidden"
+                onClick={(e) => e.stopPropagation()}
+                style={{
+                    boxShadow: "0 0 16px 1px #77777777",
+                }}
+            >
+                <div className="relative w-full h-fit flex items-center justify-between">
+                    <div className="flex text-center text-2xl font-bold p-2 tracking-widest">
+                        <h2>
+                            S: {score}
+                        </h2>
+                        {newScore && <h2 style={{ color: newScore.color }}>{`+${newScore.score}`}</h2>}
+
+                    </div>
+                    <h2
+                        className="w-48 text-center text-2xl font-bold p-2 tracking-widest"
+                        style={{
+                            position: "fixed",
+                            left: "calc(50% - 6rem)",
+                        }}
+                    >
                         - SNAKE -
                     </h2>
                     <button
@@ -164,7 +320,7 @@ export const SnakeGame: React.FC = () => {
                     </button>
                 </div>
                 <div
-                    className={`grid ${bottleEaten ? 'bg-[#590b02]' : 'bg-black'} transition-colors ease-out`}
+                    className={`grid ${latestEvent === 'bottleEaten' ? 'bg-[#590b02]' : 'bg-black'} transition-colors ease-out`}
                     style={{
                         width: GRID_WIDTH * CELL_SIZE,
                         height: GRID_HEIGHT * CELL_SIZE,
@@ -183,9 +339,8 @@ export const SnakeGame: React.FC = () => {
                                 width: CELL_SIZE,
                                 height: CELL_SIZE,
                                 position: "absolute",
-                                backgroundColor: index === 0 ? `${foodEaten ? 'white' : '#777777'}` : `${foodEaten ? '#d7b964' : '#333333'}`,
-                                borderRadius: "4px",
-                                boxShadow: foodEaten ? '0 0 8px 1px white' : 'none',
+                                backgroundColor: index === 0 ? snakeColor.head : snakeColor.body,
+                                boxShadow: latestEvent === 'foodEaten' ? '0 0 8px 1px white' : 'none',
                             }}
                         />
                     ))}
@@ -204,7 +359,7 @@ export const SnakeGame: React.FC = () => {
                     />
 
                     {/* Bottle */}
-                    {countForBottle >= 5 && (
+                    {showBottle && (
                         <FaWineBottle
                             style={{
                                 position: "absolute",
@@ -215,12 +370,32 @@ export const SnakeGame: React.FC = () => {
                             }}
                         />
                     )}
+
+                    {/* Pill */}
+                    {showPill && (
+                        <TbPillFilled
+                            className="text-red-500"
+                            style={{
+                                left: pill.x * CELL_SIZE,
+                                top: pill.y * CELL_SIZE,
+                                width: CELL_SIZE,
+                                height: CELL_SIZE,
+                                position: "absolute",
+                                borderRadius: "50%",
+                            }}
+                        />
+                    )}
                 </div>
 
-                {isGameOver && (
-                    <div className="overlay">
-                        <h3>Game Over!</h3>
-                        <button onClick={restartGame}>Restart</button>
+                {gameStatus === 'gameOver' && (
+                    <div className="absolute inset-0 z-50 flex flex-col items-center justify-center bg-white/10 backdrop-blur-md rounded-md border border-white/20 text-white">
+                        <h3 className="text-3xl font-bold mb-4">Game Over!</h3>
+                        <button
+                            onClick={restartGame}
+                            className="px-4 py-2 rounded bg-white/20 hover:bg-white/30 transition-all"
+                        >
+                            Restart
+                        </button>
                     </div>
                 )}
             </div>
